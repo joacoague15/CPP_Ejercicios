d553c9cdfa1a1fd52065acf142c868aee116931a
joacoague15 <38045464+joacoague15@users.noreply.github.com>
2020-10-12 21:53:49 -0300

    Other Exercises of HackerRank

---
+++ printNewLine.cpp
@@ -0,0 +1,25 @@
+/* Print each element on a new line in the same order it was received as input */
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+
+    cin>>a>>b>>c>>d>>e>>f;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+
+    return 0;
+}
